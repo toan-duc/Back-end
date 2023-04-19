f538dadb04a8be5f3e2b3ad71399f2393478516e
Toan <nguyenluutoanduc@gmail.com>
2023-04-19 16:49:09 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
+
 
 app.use(cors());
 app.use(express.json());
@@ -10,5 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.json({ message: "Welcome to contact book application." });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
